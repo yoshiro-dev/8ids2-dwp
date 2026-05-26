cf7026b461860d25b480b101c2e58a6a50d3be8d
YoshiZero12 <yrianoishiwara@gmail.com>
2026-05-26 15:57:16 -0600

    limpieza de carpetas

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Producto;
-
 use Illuminate\Http\Request;
 
 class ProductosController extends Controller
@@ -15,7 +14,8 @@ class ProductosController extends Controller
 
         public function index()
     {
-        return view('productos.index');
+        $productos = Producto::all();
+        return view("productos.index",compact('productos'));
     }
 
     public function store(Request $request)
@@ -30,7 +30,7 @@ class ProductosController extends Controller
         
         $producto->save();
 
-        return "Producto creado correctamente!!";
+        return redirect()->route('productos');
         
     }
 }
