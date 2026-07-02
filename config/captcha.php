5613daa8f49d0fc1d954fe8e31881f1881bd1952
Yoshiro Riaño <yrianoishiwara@gmail.com>
2026-07-02 15:30:32 -0600

    Corregir verificacion SSL de reCAPTCHA

--- config/captcha.php
+++ config/captcha.php
@@ -1,9 +1,14 @@
 <?php
 
+$gitCaBundle = 'C:\Program Files\Git\mingw64\etc\ssl\certs\ca-bundle.crt';
+$defaultVerify = PHP_OS_FAMILY === 'Windows' && file_exists($gitCaBundle) ? $gitCaBundle : true;
+$verifyCert = env('NOCAPTCHA_VERIFY_CERT');
+
 return [
     'secret' => env('NOCAPTCHA_SECRET'),
     'sitekey' => env('NOCAPTCHA_SITEKEY'),
     'options' => [
         'timeout' => 30,
+        'verify' => $verifyCert ?: $defaultVerify,
     ],
 ];
